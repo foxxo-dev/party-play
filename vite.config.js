f63f4f3002cbe05ed4e8bee70e07590b6f331ea5
Foxxo Dev <gammert54@gmail.com>
2024-01-25 10:47:02 +0100

    Added Terms And Conditions - Groundbreaking Warning 1

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,41 @@
 import topLevelAwait from 'vite-plugin-top-level-await';
-
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
+import { copy } from 'fs-extra';
 
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-        'become-host': resolve(__dirname, 'become-host/index.html'),
-        auth_process: resolve(__dirname, 'auth/process.html'),
-        auth_error: resolve(__dirname, 'auth/error.html'),
-        auth_success: resolve(__dirname, 'auth/success.html'),
-        addSong: resolve(__dirname, 'addSong/index.html')
-      }
-    }
-  },
-  plugins: [
-    topLevelAwait({
-      // The export name of top-level await promise for each chunk module
-      promiseExportName: '__tla',
-      // The function to generate import names of top-level await promise in each chunk module
-      promiseImportName: (i) => `__tla_${i}`
-    })
-  ]
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                'become-host': resolve(__dirname, 'become-host/index.html'),
+                auth_process: resolve(__dirname, 'auth/process.html'),
+                auth_error: resolve(__dirname, 'auth/error.html'),
+                auth_success: resolve(__dirname, 'auth/success.html'),
+                addSong: resolve(__dirname, 'addSong/index.html'),
+                'terms-and-conditions-us': resolve(
+                    __dirname,
+                    'terms-and-conditions/us/index.html'
+                ),
+                'terms-and-conditions-spotify': resolve(
+                    __dirname,
+                    'terms-and-conditions/spotify/index.html'
+                )
+            }
+        }
+    },
+    plugins: [
+        topLevelAwait({
+            // The export name of top-level await promise for each chunk module
+            promiseExportName: '__tla',
+            // The function to generate import names of top-level await promise in each chunk module
+            promiseImportName: (i) => `__tla_${i}`
+        }),
+        {
+            name: 'copy-assets',
+            async writeBundle() {
+                await copy(resolve(__dirname, 'assets'), resolve(__dirname, 'dist/assets'));
+            }
+        }
+    ]
 });
